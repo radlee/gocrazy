59e8a2595c7cb92bc09c52eea187908d57142a7b
radlee <prof.lolito@gmail.com>
2021-07-09 22:54:18 +0200

    Updated static root for heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join('BASE_DIR', 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join('BASE_DIR', 'media')
 MEDIA_URL = '/media/'
@@ -157,3 +158,4 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
