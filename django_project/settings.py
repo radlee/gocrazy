71269c35ffd71fcde465cf20be0ba18132613160
radlee <prof.lolito@gmail.com>
2021-07-09 23:18:13 +0200

    Added env

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,18 +15,19 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n_xu4l2@2&&a(-rvxpv#+@aj6cfdrf*(#)g346b)!0o@v+_)n*'
+# SECRET_KEY = 'django-insecure-n_xu4l2@2&&a(-rvxpv#+@aj6cfdrf*(#)g346b)!0o@v+_)n*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gocrazyexprnc.herokuapp.com']
 
 
 # Application definition
