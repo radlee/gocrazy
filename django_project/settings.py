f43c843c6df365330ef3c28614d8e439415a3a86
radlee <prof.lolito@gmail.com>
2021-07-12 12:12:56 +0200

    Settings Changed updated the site

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['myawesomedjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['gocrazyexprnc.herokuapp.com/']
 
 
 # Application definition
