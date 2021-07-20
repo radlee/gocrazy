dbb718c7c6666f6833c29d9384641f2f90381c65
radlee <prof.lolito@gmail.com>
2021-07-20 14:57:50 +0200

    Debug

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG  = 'True'
 
 ALLOWED_HOSTS = ['gocrazyexprnc.herokuapp.com/']
 
