989b02b4bc1c8aa41f2293e54983bc1708b8e60a
radlee <prof.lolito@gmail.com>
2021-07-21 16:48:24 +0200

    Updated Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG  = 'True'
+DEBUG  = True
 
 ALLOWED_HOSTS = ['gocrazyexprnc.herokuapp.com']
 
@@ -120,6 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
